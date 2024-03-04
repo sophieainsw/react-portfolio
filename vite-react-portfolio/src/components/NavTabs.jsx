6e3434bc038f8eda9d2ac00a9eecb3c09f840a67
sophieainsw <sophieainsworth3@gmail.com>
2024-03-04 20:35:18 +0000

    added react router element

---
+++ vite-react-portfolio/src/components/NavTabs.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="projects"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Projects
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ vite-react-portfolio/src/components/pages/contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Contact() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>
+       TEST TEXT
+        </p>
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
